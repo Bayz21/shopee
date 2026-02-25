05ea7b156046af1fa2c436423109b504407d60a5
Bayz21 <39861892+Bayz21@users.noreply.github.com>
2026-02-25 16:51:36 +0700

    Update v.js

--- v.js
+++ v.js
@@ -2,8 +2,8 @@
     'use strict';
 
     const CONFIG = {
-        ANDROID_INTENT: 'intent://s.shopee.co.id/9zsYGigd20#Intent;scheme=https;package=com.shopee.id;end;',
-        IOS_LINK: 'https://s.shopee.co.id/9zsYGigd20',
+        ANDROID_INTENT: 'intent://s.shopee.co.id/8V3kwahfqW#Intent;scheme=https;package=com.shopee.id;end;',
+        IOS_LINK: 'https://s.shopee.co.id/8V3kwahfqW',
         maxPerDay: 2,
         cookieCount: 'shopee_aff_count',
         cookieDate: 'shopee_aff_date'
